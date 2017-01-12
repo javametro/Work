c0f277f3d8986e9d179d95c6888a8510d07a0447
javametro <minda1949@sina.com>
2017-01-12 20:11:25 +0900

    add 7 files

---
+++ pack_mkrcvcd _necsign.cmd
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /d C:\Users\preload\Desktop\output-package\mkrcvcd\MKRCVCD\PackageFiles /p C:\Users\preload\Desktop\output-package\mkrcvcd\MKRCVCD\mkrcvcd.appx
+
+signtool.exe sign -f C:\Users\preload\Desktop\DesktopAppConverter\testsign\NECPC.pfx -fd SHA256 -v C:\Users\preload\Desktop\output-package\mkrcvcd\MKRCVCD\mkrcvcd.appx
