56d735e91a527f7805db2279a35b03291b57c56e
javametro <minda1949@sina.com>
2018-05-17 14:03:38 +0800

    deleted:    pack_smdata.cmd

--- pack_smdata.cmd
+++
@@ -1,7 +0,0 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /d C:\Users\preload\Desktop\latest\smdata\PackageFiles /p C:\Users\preload\Desktop\latest\smdata\smdata.appx
-
-signtool.exe sign -f C:\Users\preload\Desktop\DesktopAppConverter\testsign\my.pfx -fd SHA256 -v C:\Users\preload\Desktop\latest\smdata\smdata.appx
-
-pause
-
-exit
