a492eb5d0b242f825c921a5671b8a5199940aa0f
javametro <minda1949@sina.com>
2018-05-17 14:02:46 +0800

    deleted:    pack_mkrcvcd.cmd

--- pack_mkrcvcd.cmd
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /d C:\Users\preload\Desktop\output-package\mkrcvd\MKRCVCD\PackageFiles /p C:\Users\preload\Desktop\output-package\mkrcvd\MKRCVCD\mkrcvcd.appx
-
-signtool.exe sign -f C:\Users\preload\Desktop\DesktopAppConverter\testsign\my.pfx -fd SHA256 -v C:\Users\preload\Desktop\output-package\mkrcvd\MKRCVCD\mkrcvcd.appx
